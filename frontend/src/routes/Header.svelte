48bbd6c935df2772c49cdb3d77d662fa4220be3f
Brady D <bradydriebergen@outlook.com>
2025-10-30 23:42:47 -0600

    Started frontend work

---
+++ frontend/src/routes/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<header>
+	<img class="game-logo" src="/logos/TerrariadleLogo.png" alt="Terrariadle" />
+</header>
+
+<style>
+	.game-logo {
+		width: 400px;
+		max-width: 80%;
+		height: auto;
+	}
+</style>
