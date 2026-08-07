2938db2a314316c8852e566108808e10c4106152
bradydriebergen <142831669+BradyDriebergen@users.noreply.github.com>
2026-08-07 14:55:43 -0600

    Fixed sitemap.xml

--- frontend/src/routes/sitemap.xml/+server.ts
+++ frontend/src/routes/sitemap.xml/+server.ts
@@ -13,11 +13,10 @@ const routes = [
 ];
 
 export const GET: RequestHandler = async () => {
-	const body = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${routes.map((r) => `  <url><loc>https://terrariadle.com${r}</loc></url>`).join('\n')}
-        </urlset>`;
+	const body = `<?xml version="1.0" encoding="UTF-8"?>
+	<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+	${routes.map((r) => `  <url><loc>https://terrariadle.com${r}</loc></url>`).join('\n')}
+	</urlset>`;
 
 	return new Response(body, {
 		headers: { 'Content-Type': 'application/xml' }
